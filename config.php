ce44928145d0b13d81c7bce4630ad4180104b1b2
lixiaopei <lixiaopei@yaochufa.com>
2017-07-24 17:57:55 +0800

    配置文件优化

--- config.php
+++ config.php
@@ -6,13 +6,13 @@ return [
     'logPath'   => __DIR__ . '/log',
     'exec'      => [
         [
-            'token'     => 'kcloze-test-1',
+            'name'     => 'kcloze-test-1',
             'bin'       => '/usr/bin/php',
             'binArgs'   => [__DIR__ . '/test.php', 'oop', '123'],
             'workNum'   => 3,
         ],
         [
-            'token'     => 'kcloze-test-2',
+            'name'     => 'kcloze-test-2',
             'bin'       => '/usr/bin/php',
             'binArgs'   => [__DIR__ . '/test2.php', 'oop', '456'],
             'workNum'   => 5,
